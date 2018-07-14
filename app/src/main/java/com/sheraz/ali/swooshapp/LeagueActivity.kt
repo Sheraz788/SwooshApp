8a23e7698e49d0d3942bcad0e728a0c1980549a8
Sheraz Ali <Sherazalibk@gmail.com>
2018-07-14 16:01:58 -0700

    changes in toggle buttons

--- app/src/main/java/com/sheraz/ali/swooshapp/LeagueActivity.kt
+++ app/src/main/java/com/sheraz/ali/swooshapp/LeagueActivity.kt
@@ -1,12 +1,21 @@
 package com.sheraz.ali.swooshapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BasicActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClicked(view: View){
+
+        val intiateSkillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(intiateSkillActivity)
+
     }
 }
