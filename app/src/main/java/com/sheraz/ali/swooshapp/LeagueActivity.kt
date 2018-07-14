faf3ff0a36cf214540cc6ec79e5ab5fc0cdd51af
Sheraz Ali <Sherazalibk@gmail.com>
2018-07-14 16:58:57 -0700

    working on Toggle buttons in League and Skill Activity

--- app/src/main/java/com/sheraz/ali/swooshapp/LeagueActivity.kt
+++ app/src/main/java/com/sheraz/ali/swooshapp/LeagueActivity.kt
@@ -4,18 +4,53 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BasicActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
+    fun onMensClicked(view: View){
+
+        womensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = mensLeagueBtn.text.toString()
+
+    }
+    fun onWomensClicked(view: View){
+
+        mensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = womensLeagueBtn.text.toString()
+
+    }
+    fun onCoedClicked(view: View){
+
+        mensLeagueBtn.isChecked = false
+        womensLeagueBtn.isChecked = false
+
+        selectedLeague = coedLeagueBtn.text.toString()
+
+    }
+
     fun leagueNextClicked(view: View){
 
-        val intiateSkillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(intiateSkillActivity)
+        if(selectedLeague != ""){
+            val intiateSkillActivity = Intent(this, SkillActivity::class.java)
+            intiateSkillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(intiateSkillActivity)
+        }else{
+            Toast.makeText(this, "Please select one League.", Toast.LENGTH_SHORT).show()
+        }
+
 
     }
 }
---
+++ app/src/main/java/com/sheraz/ali/swooshapp/app_constants.kt
@@ -0,0 +1,7 @@
+package com.sheraz.ali.swooshapp
+
+/**
+ * Created by Mega Tech on 7/14/2018.
+ */
+
+const val EXTRA_LEAGUE = "league"
